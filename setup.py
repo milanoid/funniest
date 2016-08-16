f40334386d823fce32f499e2c130d7b7e612503e
Milan Vojnovič <vojnovic@avast.com>
2016-08-16 22:02:01 +0200

    Minimal Structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='funniest',
+    version='0.1',
+    packages=['funniest'],
+    url='https://github.com/milanoid/funniest.git',
+    license='',
+    author='Milan Vojnovic',
+    author_email='milan.vojnovic@gmail.com',
+    description='The funniest joke in the world'
+)
