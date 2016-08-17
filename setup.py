11c8ca8d9425eb563265441625f47bf1d287a570
milanoid <milanvojnovic@gmail.com>
2016-08-17 12:27:24 +0200

    additional metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
+      long_description='Really, the funniest around.',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke commedy flying circus',
       url='https://github.com/milanoid/funniest.git',
       author='Milan Vojnovic',
       author_email='milan.vojnovic@gmail.com',
@@ -11,5 +19,5 @@ setup(name='funniest',
       install_requires=[
           'markdown',
       ],
+      include_package_data=True,
       zip_safe=False)
-
